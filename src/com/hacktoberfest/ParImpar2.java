52ec8f96340d8049b0432277d56a3b9e8f2cb6c9
Abel Gonzalez <fosquiabelgonzalezmartinez@gmail.com>
2016-10-28 13:29:20 +0200

    He corregido el nombre de la calse ParImpar

--- src/com/hacktoberfest/ParImpar2.java
+++ src/com/hacktoberfest/ParImpar2.java
@@ -2,7 +2,7 @@ package com.hacktoberfest;
 
 import java.util.Scanner;
 
-public class ParImpar {
+public class ParImpar2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
